22df59a07e6e3f8ed360134ade5824073bb796fb
robindehullu-howest <robin.dehullu@student.howest.be>
2023-10-19 19:09:49 +0200

    Completed 07

---
+++ Exercise07/Program.cs
@@ -0,0 +1,9 @@
+﻿List<int> listA = new List<int> { 1, 2, 3, 4, 5 };
+List<int> listB = new List<int> { 4, 5, 6, 7, 8 };
+
+var result = listA.Intersect(listB).ToList();
+
+foreach (var item in result)
+{
+    Console.WriteLine(item);
+}
