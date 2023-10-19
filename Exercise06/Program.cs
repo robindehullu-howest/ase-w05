8a7e7dd501e495aac573ad19815c064294f9a213
robindehullu-howest <robin.dehullu@student.howest.be>
2023-10-19 19:08:34 +0200

    Complete 06

---
+++ Exercise06/Program.cs
@@ -0,0 +1,19 @@
+﻿Random random = new Random();
+List<int> randomNumbers = new List<int>();
+for (int i = 0; i < 10; i++)
+{
+    randomNumbers.Add(random.Next(1, 100));
+}
+
+List<int> randomNumbers2 = new List<int>();
+for (int i = 0; i < 10; i++)
+{
+    randomNumbers2.Add(random.Next(1, 100));
+}
+
+var mergedList = randomNumbers.Union(randomNumbers2);
+
+foreach (var item in mergedList)
+{
+    Console.WriteLine(item);
+}
