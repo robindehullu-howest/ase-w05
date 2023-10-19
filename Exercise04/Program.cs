2afa6c37167b536baf7ed3c99a2feac6a6387dc6
robindehullu-howest <robin.dehullu@student.howest.be>
2023-10-19 17:26:54 +0200

    Completed ex04

--- Exercise04/Program.cs
+++ Exercise04/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿List<string> cities = new List<string>
+{
+    "London",
+    "Paris",
+    "Milan",
+    "New York",
+    "Los Angeles"
+};
+
+Console.WriteLine("Cities:");
+Console.WriteLine(string.Join(", ", cities));
+
+var citiesStartingWithL = from city in cities
+                              where city.StartsWith("L")
+                              orderby city
+                              select city;
+
+Console.WriteLine("Cities starting with L:");
+Console.WriteLine(string.Join(", ", citiesStartingWithL));
+
+var citiesStartingWithLAndEndingWithN = from city in cities
+                                         where city.StartsWith("L") && city.EndsWith("n")
+                                         orderby city
+                                         select city;
+
+Console.WriteLine("Cities starting with L and ending with n:");
+Console.WriteLine(string.Join(", ", citiesStartingWithLAndEndingWithN));
+
+var citiesWithLengthOf6 = from city in cities
+                             where city.Length == 6
+                             orderby city
+                             select city;
+
+Console.WriteLine("Cities with length of 6:");
+Console.WriteLine(string.Join(", ", citiesWithLengthOf6));
+
